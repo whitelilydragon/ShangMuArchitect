f3786ce9ebb47ef9b7f92323dda2fbe44fa90aa7
Vela Cosmos <whitelilydragon@users.noreply.github.com>
2024-06-11 19:43:34 -0400

    Update scr_lang_lagoon_us_en.c

--- Localization/scr_lang_lagoon_us_en.c
+++ Localization/scr_lang_lagoon_us_en.c
@@ -9,6 +9,7 @@ o.display_page_description[2] = "Slopes"
 o.display_page_description[3] = "Outdoors"
 o.display_page_description[4] = "Scenery 1"
 o.display_page_description[5] = "Scenery 2"
+o.display_page_description[6] = "Extra 1"
 
 //Tile Descriptions (Page 1)
 o.display_description_item[o.display_page_one,0] = "Sand - Surface Edge Left"
