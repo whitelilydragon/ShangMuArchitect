d8418f5ef31d9673c80da38b0d45bcbf867e9a6e
Vela Cosmos <whitelilydragon@users.noreply.github.com>
2024-07-03 22:44:58 -0400

    Update scr_lang_lagoon_es_es.c

--- Localization/scr_lang_lagoon_es_es.c
+++ Localization/scr_lang_lagoon_es_es.c
@@ -10,6 +10,7 @@ o.display_page_description[3] = "Exterior"
 o.display_page_description[4] = "Decoración 1"
 o.display_page_description[5] = "Decoración 2"
 o.display_page_description[6] = "Extra 1"
+o.display_page_description[7] = "Pier"
 
 //Tile Descriptions (Page 1)
 o.display_description_item[o.display_page_one,0] = "Arena - Esquina Superficial Izquierda"
