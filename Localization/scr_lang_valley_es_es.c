2a385770314fbc532efbeebf96af59d58fa62b73
Vela Cosmos <whitelilydragon@users.noreply.github.com>
2026-02-04 10:48:15 -0500

    Create scr_lang_valley_es_es.c

---
+++ Localization/scr_lang_valley_es_es.c
@@ -0,0 +1,80 @@
+///Shang Mu Temple Tile Definition Table
+var o = obj_level_editor;
+
+//Short Hand Alias For Each Page
+o.display_page_one = 0
+o.display_page_two = 1
+o.display_page_three = 2
+o.display_page_max = 2
+
+//Page Descriptions
+o.display_page_description[0] = "Platform"
+o.display_page_description[1] = "Slopes"
+o.display_page_description[2] = "Scenery"
+var str_unknown_tile = "Unknown Tile";
+
+//Tile Descriptions (Page 1)
+o.display_description_item[o.display_page_one,0] = "Edge Platform 1"
+o.display_description_item[o.display_page_one,1] = "Valley Floor 1"
+o.display_description_item[o.display_page_one,2] = "Edge Platform 2"
+o.display_description_item[o.display_page_one,3] = "Valley Wall 1"
+o.display_description_item[o.display_page_one,4] = "Grassy Edge Platform 1"
+o.display_description_item[o.display_page_one,5] = "Grassy Valley Floor 1"
+o.display_description_item[o.display_page_one,6] = "Grassy Edge Platform 2"
+o.display_description_item[o.display_page_one,7] = "Valley Wall 2"
+o.display_description_item[o.display_page_one,8] = "Valley Wall 3"
+o.display_description_item[o.display_page_one,9] = "Valley Wall 4"
+o.display_description_item[o.display_page_one,10] = "Grassy Valley Floor 2"
+o.display_description_item[o.display_page_one,11] = "Valley Floor 2"
+o.display_description_item[o.display_page_one,12] = "Floating Stone Platform"
+o.display_description_item[o.display_page_one,13] = "Valley Floor 3"
+o.display_description_item[o.display_page_one,14] = "Valley Floor 4"
+o.display_description_item[o.display_page_one,15] = "Valley Floor 5"
+o.display_description_item[o.display_page_one,16] = "Valley Floor 6"
+o.display_description_item[o.display_page_one,17] = str_unknown_tile
+o.display_description_item[o.display_page_one,18] = str_unknown_tile
+o.display_description_item[o.display_page_one,19] = str_unknown_tile
+
+//Tile Descriptions (Page 2)
+o.display_description_item[o.display_page_two,0] = "Valley Ramp 1"
+o.display_description_item[o.display_page_two,1] = "Valley Ramp 2"
+o.display_description_item[o.display_page_two,2] = "Valley Ramp 3"
+o.display_description_item[o.display_page_two,3] = "Valley Ramp 4"
+o.display_description_item[o.display_page_two,4] = "Valley Ramp 5"
+o.display_description_item[o.display_page_two,5] = "Valley Ramp 6"
+o.display_description_item[o.display_page_two,6] = str_unknown_tile
+o.display_description_item[o.display_page_two,7] = "Valley Loop 1"
+o.display_description_item[o.display_page_two,8] = "Valley Loop 2"
+o.display_description_item[o.display_page_two,9] = "Valley Hill 1"
+o.display_description_item[o.display_page_two,10] = "Valley Hill 2"
+o.display_description_item[o.display_page_two,11] = "Valley Hill 3"
+o.display_description_item[o.display_page_two,12] = "Valley Hill 4"
+o.display_description_item[o.display_page_two,13] = "Valley Ceiling 1"
+o.display_description_item[o.display_page_two,14] = "Valley Ceiling 2"
+o.display_description_item[o.display_page_two,15] = "Valley Ceiling 3"
+o.display_description_item[o.display_page_two,16] = "Valley Ceiling 4"
+o.display_description_item[o.display_page_two,17] = str_unknown_tile
+o.display_description_item[o.display_page_two,18] = str_unknown_tile
+o.display_description_item[o.display_page_two,19] = str_unknown_tile
+
+//Tile Descriptions (Page 3)
+o.display_description_item[o.display_page_three,0] = "Valley Stone Pile 1"
+o.display_description_item[o.display_page_three,1] = "Valley Stone Pile 2"
+o.display_description_item[o.display_page_three,2] = "Valley Stone Pile 3"
+o.display_description_item[o.display_page_three,3] = "Valley Tree Leaves 1"
+o.display_description_item[o.display_page_three,4] = "Valley Tree Leaves 2"
+o.display_description_item[o.display_page_three,5] = "Valley Tree"
+o.display_description_item[o.display_page_three,6] = "Valley Wall Decals 1"
+o.display_description_item[o.display_page_three,7] = "Valley Wall Decals 2"
+o.display_description_item[o.display_page_three,8] = "Valley Wall Decals 3"
+o.display_description_item[o.display_page_three,9] = "Valley Wall Decals 4"
+o.display_description_item[o.display_page_three,10] = "Valley Wall Background"
+o.display_description_item[o.display_page_three,11] = "Valley Stone Background"
+o.display_description_item[o.display_page_three,12] = str_unknown_tile
+o.display_description_item[o.display_page_three,13] = str_unknown_tile
+o.display_description_item[o.display_page_three,14] = str_unknown_tile
+o.display_description_item[o.display_page_three,15] = str_unknown_tile
+o.display_description_item[o.display_page_three,16] = str_unknown_tile
+o.display_description_item[o.display_page_three,17] = str_unknown_tile
+o.display_description_item[o.display_page_three,18] = str_unknown_tile
+o.display_description_item[o.display_page_three,19] = str_unknown_tile
